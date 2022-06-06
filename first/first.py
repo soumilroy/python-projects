68c7c208435adf812c9b5e70df88e1e486f27af1
Soumil Roy <hello@soumilroy.com>
2022-06-06 16:31:32 +0530

    first commit

---
+++ first/first.py
@@ -0,0 +1,28 @@
+print (1+2+3)
+print ("Hello Python!")
+
+# Strings
+string = "Hello Python!"
+print (string[2:9])
+print ("********END OF PROGRAM********")
+
+# Escape characters
+# Examples: \n, \t, \b, \r, \f, \v, \", \', \\
+
+print ("Hello \nPython!") # New line
+print ("Hello \t\tPython!") # Tab
+print ("Hello \bPython!") # Backspace
+print ("Hello \rPython!") # Carriage return
+print ("Hello \fPython!") # Form feed
+print ("Hello \vPython!") # Vertical tab
+print ("\"Hello Python!\"") # Double quotes
+print ("Hello \\Python!") # Backslash
+
+message = """
+	A quick brown fox 
+	jumps over the lazy dog.
+	Lorem ipsum dolor sit amet, 
+	consectetur adipiscing elit.
+"""
+
+print (message)
