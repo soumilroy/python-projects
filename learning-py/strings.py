8e4cf39276e83efd67592f0aaf9378011db2c07f
Soumil Roy <hello@soumilroy.com>
2022-06-07 18:27:11 +0530

    slicing, concat, index

--- learning-py/strings.py
+++ learning-py/strings.py
@@ -1,6 +1,48 @@
-print (1+2+3)
-print ("Hello Python!")
+website = "https://soumilroy.com is      my site"
+print (website)
 
+message = "I'm a programmer"
+print (message)
+
+message2 = 'I\'m a programmer'
+print (message2)
+
+# Concatenation
+print ("Darth" + " " + "Vader")
+
+# Multiplication
+meme = "lo"
+print (meme * 6)
+
+# String indexes 
+name = "Soumil"
+print (name[0]);
+print (name[1]);
+print (name[2]);
+print (name[3]);
+print (name[4]);
+print (name[5]);
+
+print (name[0] + name[3]);
+
+# Slicing strings
+message = "Hello Python!"
+print (message[6:15]);
+print (message[6:-1]);
+print (message[-15:-8]) # Slicing from EOS
+print (message[4:]) # from index 4 till the end 'o Python!'
+print (message[:6]) # from index 0 till 6 'Hello '
+
+# Get characters from EOS
+print (name[-1]);
+print (name[-2]);
+
+# Index that doesn't exist
+# print (name[10]);
+
+# Slicing with steps
+print (message[2:10:3]) # 3 being the skip step
+print (message[2:15:2]) # skip every 2 chars
 # Strings
 string = "Hello Python!"
 print (string[2:9])
@@ -26,3 +68,9 @@ message = """
 """
 
 print (message)
+
+# String length function
+print (len(message)) # 107
+
+# print (len(122))
+print (len("")) # 0
