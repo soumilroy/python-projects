e51e6780a447c0223c909e8cc0ce0d89f3993d63
Soumil Roy <hello@soumilroy.com>
2022-06-08 02:31:20 +0530

    input fn

---
+++ input-fn.py
@@ -0,0 +1,10 @@
+name = input ("Enter your name: ")
+print ("Welcome to the matrix, " + name + "!")
+
+age = input ("Enter your age: ")
+print ("You are " + age + " years old.")
+
+age = int(age);
+
+print ("You will be " + str(float(age + 10)) + " years old in the year " + str(2022 + 10) + ".")
+
