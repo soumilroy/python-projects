228f026c161e14547b28b6e46862822ddad6c930
Soumil Roy <hello@soumilroy.com>
2022-06-12 13:27:26 +0530

    conditionals & bools

---
+++ learning-py/conditionals-bools.py
@@ -0,0 +1,18 @@
+print (4 == 3) # False
+print (4 != 3) # True
+print ("soumil" == "soumil") # True
+
+print ("4" == 4) # False
+# print ("10" <= 10) # Error
+
+print (5.6 > 5.4) # True
+print (1.23456789 > 1.23456788) # True
+print (-1.23456789 < -1.23456788) # True
+print (11.000001 > 11.0) # False
+
+print (7.0 == 7) # True ? but why? coersion?
+print (10.0 == 10.0) # True
+print (11.000001 == 11.0) # False
+print (12.3 != 12.3) # False
+
+# Truthy & Falsey values
