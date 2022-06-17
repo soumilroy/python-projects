c29f133045c679838cafc06c23697485b06fe8c2
Soumil Roy <hello@soumilroy.com>
2022-06-17 10:10:06 +0530

    more loops

---
+++ learning-py/break.py
@@ -0,0 +1,9 @@
+name = "Soumil Roy"
+total_vowels = 0
+
+for char in name:
+		if (char in "R"):
+			break;
+		print (char)	
+  # S, o, u, m, i, l 
+  # Skips from R onwards
---
+++ learning-py/continue.py
@@ -0,0 +1,5 @@
+for char in "SOUMIL":
+		if char in "AEIOU":
+			continue
+		
+		print (char) # Skip all vowels
