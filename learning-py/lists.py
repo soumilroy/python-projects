6e23baea8b676c642606570aaddf8795c1d19870
Soumil Roy <hello@soumilroy.com>
2022-06-27 17:08:40 +0530

    lists

---
+++ learning-py/lists.py
@@ -0,0 +1,15 @@
+# similar to arrays, Lists are ordered collection of values
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print (numbers) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (numbers[3]) # 4
+print (numbers[-1]) # 10
+print (numbers[3:6]) # [4, 5, 6]
+
+# Lists can have any type of data
+
+rand_data = [1, "soumil", True, 3.14, []]
+print (rand_data) # [1, 'soumil', True, 3.14, []]
+
+print (list('SOUMIL')) # ['S', 'O', 'U', 'M', 'I', 'L']
