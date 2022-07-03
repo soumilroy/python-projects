1eb9cdd59088392a53604d29dd66f69556d8e4c4
Soumil Roy <hello@soumilroy.com>
2022-07-03 10:48:10 +0530

    loop for else

--- learning-py/loops.py
+++ learning-py/loops.py
@@ -20,3 +20,13 @@ while roll1 != 1 or roll2 != 1:
     count += 1
 
 print("You rolled " + str(count) + " rolls.")
+
+# prime numbers in a range from 2 to 20
+
+for n in range(2, 50):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
