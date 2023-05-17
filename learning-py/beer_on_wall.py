0abd779bd348b0f9d7ce8666ba3b53f24fe1d18c
Soumil Roy <hello@soumilroy.com>
2023-05-17 11:01:30 +0530

    experiments

--- learning-py/beer_on_wall.py
+++ learning-py/beer_on_wall.py
@@ -4,4 +4,7 @@ for beer in beer_count:
     if beer == 1:
         print(f"Take one down and pass it around, no more bottles of beer on the wall.")
     else:
-        print(f"Take one down and pass it around, {beer - 1} bottles of beer on the wall.")
+        print(f"""Take 
+        one down and 
+        pass it around, {beer - 1} bottles 
+        of beer on the wall.""")
